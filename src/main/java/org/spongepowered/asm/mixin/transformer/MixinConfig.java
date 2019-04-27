b5dde319e7d2ac67758cd64c34210f7c46f97267
Adrian Siekierka <kontakt@asie.pl>
2019-04-27 21:31:37 +0200

    [Fabric] lower minVersion message to warning level; it is not always an error condition

--- src/main/java/org/spongepowered/asm/mixin/transformer/MixinConfig.java
+++ src/main/java/org/spongepowered/asm/mixin/transformer/MixinConfig.java
@@ -406,7 +406,7 @@ final class MixinConfig implements Comparable<MixinConfig>, IMixinConfig {
 
     private boolean checkVersion() throws MixinInitialisationError {
         if (this.version == null) {
-            this.logger.error("Mixin config {} does not specify \"minVersion\" property", this.name);
+            this.logger.warn("Mixin config {} does not specify \"minVersion\" property", this.name);
         }
         
         VersionNumber minVersion = VersionNumber.parse(this.version);
